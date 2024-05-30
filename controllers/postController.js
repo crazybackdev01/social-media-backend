6755929b67af2a0249c1f21e6ba4040def4e8dd2
Udit Nagar <uditnagar419@gmail.com>
2024-05-30 09:55:44 +0530

    Post controller started

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,2 +1,38 @@
 const Post = require("../models/Post.model.js");
 const { CustomError } = require("../middlewares/error.js");
+const User = require("../models/User.model.js");
+
+//Create Post with only caption
+const createPostController = async (req, res, next) => {
+  const { userId, caption } = req.params;
+  if (!userId || !caption) {
+    throw new CustomError("Provide caption or userId not detected", 400);
+  }
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new CustomError("User not found", 400);
+    }
+    const newPost = new Post({
+      user: userId,
+      caption,
+    });
+
+    if (!newPost) {
+      throw new CustomError("Please try again!", 500);
+    }
+
+    await newPost.save();
+    user.posts.push(newPost);
+
+    return res.status(200).json({
+      success: true,
+      message: "Post uploaded successfully",
+      post: newPost,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Create post with images and caption
